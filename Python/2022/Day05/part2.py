61998b0428cc6a2013b7dcb6742fdb16fb7d8289
einfachIrgendwer0815 <85333734+einfachIrgendwer0815@users.noreply.github.com>
2022-12-09 15:22:24 +0100

    Python 2022 Day 5

---
+++ Python/2022/Day05/part2.py
@@ -0,0 +1,37 @@
+file = "input.txt"
+
+def setup_stacks(stack_lines, stacks=9):
+    stacks = [[] for _ in range(9)]
+
+    for line in stack_lines:
+        for stack_index, line_index in enumerate(range(1, len(line), 4)):
+            if line[line_index] != " " and line[line_index] != "\n":
+                stacks[stack_index].append(line[line_index])
+
+    for stack in stacks:
+        stack.reverse()
+
+    return stacks
+
+if __name__ == "__main__":
+    stack_lines = []
+    stacking = True
+
+    stacks = None
+
+    with open(file, "r") as open_file:
+        while (line := open_file.readline()):
+            if stacking:
+                stack_lines.append(line)
+                if line.strip() == "":
+                    stacking = False
+                    stacks = setup_stacks(stack_lines)
+
+                continue
+
+            move = list(map(int, line.strip().split(" ")[1::2]))
+
+            containers = [stacks[move[1] - 1].pop() for _ in range(move[0])]
+            stacks[move[2] - 1] += containers[::-1] # [::-1] to retain the order of the containers
+
+    print(''.join(stack[-1] for stack in stacks))
